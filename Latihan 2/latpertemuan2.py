44fdc23edb4125f02a6c24210092d08d268712cb
Firdaus2106 <firdausdedefirdaus88@gmail.com>
2026-09-16 10:51:18 +0700

    Add files via upload

---
+++ Latihan 2/latpertemuan2.py
@@ -0,0 +1,15 @@
+# ==========================================
+# PROGRAM PENGENALAN VARIABEL PYTHON
+# ==========================================
+
+# membuat variabel
+nama = "Muhammad Dede Yusuf Firdaus"
+umur = 18
+tinggi = 173.1
+sudah_menikah = False
+
+# Menampilkan nilai dan tipe variabel
+print("Nama :" , nama, "| Tipe :", type(nama))
+print("Umur :" , umur, "| Tipe :", type(umur))
+print("Tinggi :" , tinggi, "| Tipe :", type(tinggi))
+print("Sudah Menikah :" , sudah_menikah, "| Tipe :", type(sudah_menikah))
